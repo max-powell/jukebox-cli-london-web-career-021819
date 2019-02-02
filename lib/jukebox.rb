2ef3feaaf4e6480cc4067015d7ca791ccfb06ba2
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-02 15:58:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
+  binding.pry
   if user_input <= songs.length || songs.include?(user_input)
-    binding.pry
   end
 end
